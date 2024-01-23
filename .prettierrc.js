1e409b5fda8e709905922000af44076ba1228d4b
Hyeonmeoung Joo <wugusaud77@icloud.com>
2024-01-23 23:39:35 +0900

    Init: 프로젝트 셋업

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,8 @@
 module.exports = {
-  arrowParens: 'avoid',
-  bracketSameLine: true,
-  bracketSpacing: false,
-  singleQuote: true,
-  trailingComma: 'all',
-};
+  "singleQuote": true,
+  "semi": true,
+  "useTabs": false,
+  "tabWidth": 2,
+  "trailingComma": "all",
+  "printWidth": 80
+}
--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,33 @@
 module.exports = {
-  presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    [
+      'module:react-native-dotenv',
+      {
+        moduleName: '@env',
+        path: '.env',
+        blacklist: null,
+        whitelist: null,
+        safe: true,
+        allowUndefined: true,
+      },
+    ],
+    'babel-plugin-styled-components',
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        alias: {
+          '@': './src',
+          '@components': './src/components',
+          '@screens': './src/screens',
+          '@navigation': './src/navigation',
+          '@assets': './src/assets',
+          '@lib': './src/lib',
+          '@types': './src/types',
+          '@hooks': './src/hooks',
+        },
+      },
+    ],
+  ],
+  presets: ['module:metro-react-native-babel-preset'],
 };
