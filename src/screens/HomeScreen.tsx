3a1b244acf8bea7e09e762880671263df86b336b
Hyeonmeoung Joo <wugusaud77@icloud.com>
2024-01-23 23:50:02 +0900

    feat: screen 파일 생성

---
+++ src/screens/HomeScreen.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Animated } from 'react-native';
+import View = Animated.View;
+
+const HomeScreen = () => {
+  return <View></View>;
+};
+
+export default HomeScreen;
---
+++ src/screens/LoginScreen.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Animated } from 'react-native';
+import View = Animated.View;
+
+const LoginScreen = () => {
+  return <View></View>;
+};
+
+export default LoginScreen;
