535ceee74e0f9b88fa89e3d3a970adb2e51b2e8d
paul3083 <paul30833083@gmail.com>
2024-01-24 01:33:56 +0900

    Theme

---
+++ src/theme/dark.ts
@@ -0,0 +1,15 @@
+import Palette from '@/theme/types/Palette';
+
+const dark: Palette = <Palette>{
+  gray10: '#1B1B21',
+  gray20: '#15151B',
+  gray30: '#777693',
+  gray40: '#9A9AB0',
+  gray50: '#B0B0C1',
+  gray60: '#CACADE',
+  gray70: '#E4E8F2',
+  gray80: '#EBEBF2',
+  highlight: '#3671C2',
+};
+
+export default dark;
