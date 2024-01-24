daf58420aac664372eebb34b305f3fbd2544965c
Doyun Yu <doyun0109@icloud.com>
2024-01-24 10:48:23 +0900

    Fix: ThemeProvider styled-components -> styled-components/native

--- App.tsx
+++ App.tsx
@@ -3,7 +3,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { RecoilRoot } from 'recoil';
 import RootNavigator from './src/navigation/RootNavigator';
 import React from 'react';
-import { ThemeProvider } from 'styled-components';
+import { ThemeProvider } from 'styled-components/native';
 import { light } from '@/theme';
 
 const queryClient = new QueryClient();
