0c69b185fc4addab487e26ff9fc30a1159c6b854
paul3083 <paul30833083@gmail.com>
2024-01-25 22:48:41 +0900

    Style: 코드 탭 간격 수정

--- src/components/atomic/Container.tsx
+++ src/components/atomic/Container.tsx
@@ -15,23 +15,23 @@ interface ContainerProps {
 }
 
 const base = styled.View<ContainerProps>`
-    display: flex;
-    gap: ${props => props.$gap || 0}px;
-    justify-content: ${props => props.$justifyContent || 'flex-start'};
-    align-items: ${props => props.$alignItems || 'flex-start'};
-    padding: ${props =>
-            `${props.$padding ? props.$padding[0] : 0}px ${
-                    props.$padding ? props.$padding[1] ?? props.$padding[0] : 0
-            }px`};
-    flex-wrap: ${props => (props.$wrap ? 'wrap' : 'nowrap')};
-    background-color: ${props => props.$backgroundColor || 'transparent'};
-    width: ${props => (props.$fill ? '100%' : 'auto')};
-    height: ${props => (props.$height ? `${props.$height}px` : 'auto')};
+  display: flex;
+  gap: ${(props) => props.$gap || 0}px;
+  justify-content: ${(props) => props.$justifyContent || 'flex-start'};
+  align-items: ${(props) => props.$alignItems || 'flex-start'};
+  padding: ${(props) =>
+    `${props.$padding ? props.$padding[0] : 0}px ${
+      props.$padding ? props.$padding[1] ?? props.$padding[0] : 0
+    }px`};
+  flex-wrap: ${(props) => (props.$wrap ? 'wrap' : 'nowrap')};
+  background-color: ${(props) => props.$backgroundColor || 'transparent'};
+  width: ${(props) => (props.$fill ? '100%' : 'auto')};
+  height: ${(props) => (props.$height ? `${props.$height}px` : 'auto')};
 `;
 
 export const Column = styled(base)`
-    flex-direction: column;
+  flex-direction: column;
 `;
 export const Row = styled(base)`
-    flex-direction: row;
+  flex-direction: row;
 `;
