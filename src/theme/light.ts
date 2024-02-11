174a74054695eab73e7ccb2872ce8d9bf60321da
Hyeonmeoung Joo <wugusaud77@icloud.com>
2024-02-12 02:18:13 +0900

    Chore: 컬러 팔레트 수정

--- src/theme/light.ts
+++ src/theme/light.ts
@@ -2,7 +2,7 @@ import Palette from '@/theme/types/Palette';
 
 const light: Palette = {
   gray10: '#FFFFFF',
-  gray20: '#F0F2F5',
+  gray20: '#F0F2F4',
   gray30: '#E7EAEF',
   gray40: '#B5BECB',
   gray50: '#98A1AE',
