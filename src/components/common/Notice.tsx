df1e1b9a9e37de61ff814e8eee5e87905610c901
Hyeonmeoung Joo <wugusaud77@icloud.com>
2024-02-12 18:29:34 +0900

    Feat: 공지 컴포넌트 작성

---
+++ src/components/common/Notice.tsx
@@ -0,0 +1,73 @@
+import React, { useState } from 'react';
+import styled, { useTheme } from 'styled-components/native';
+import Typography from '../typography';
+import ArrowDown from '@assets/icons/down.svg';
+
+interface NoticeProps {
+  title: string;
+  content: string;
+  date: string;
+}
+
+const Notice = ({ title, content, date }: NoticeProps) => {
+  const { colors } = useTheme();
+  const [isOpen, setIsOpen] = useState(false);
+  return (
+    <NoticeLayout>
+      <NoticeTop>
+        <NoticeTopLeft>
+          <Typography.SemiLabel $color={colors.gray90}>
+            {title}
+          </Typography.SemiLabel>
+          <Typography.Caption $color={colors.gray60}>{date}</Typography.Caption>
+        </NoticeTopLeft>
+        <IconBox onPress={() => setIsOpen(!isOpen)}>
+          <ArrowDown fill={colors.gray80} />
+        </IconBox>
+      </NoticeTop>
+      {isOpen ? (
+        <Typography.Body $color={colors.gray80}>{content}</Typography.Body>
+      ) : (
+        <></>
+      )}
+    </NoticeLayout>
+  );
+};
+
+const NoticeTopLeft = styled.View`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
+  gap: 4px;
+`;
+
+const IconBox = styled.TouchableOpacity`
+  width: 24px;
+  height: 24px;
+  justify-content: center;
+  align-items: center;
+`;
+
+const NoticeLayout = styled.View`
+  display: flex;
+  padding: 20px;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
+  gap: 16px;
+  align-self: stretch;
+  border-radius: 8px;
+  border: 1px solid ${({ theme }) => theme.colors.gray30};
+  background: ${({ theme }) => theme.colors.gray10};
+`;
+
+const NoticeTop = styled.View`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  align-self: stretch;
+`;
+
+export default Notice;
