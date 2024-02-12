63549b54cc9c9f622278965fa343d33f2c3ad886
Hyeonmeoung Joo <wugusaud77@icloud.com>
2024-02-12 16:50:18 +0900

    Fix: lineheight 동적 값 적용

--- src/components/typography/SemiBold.tsx
+++ src/components/typography/SemiBold.tsx
@@ -2,32 +2,31 @@ import styled from 'styled-components/native';
 import { Text } from '@components/atomic';
 import { getFontSize } from '@lib/fonts';
 
-
 const SemiBold = styled(Text)`
   font-weight: 600;
 `;
 
 export const Name = styled(SemiBold)`
   font-size: ${getFontSize(32)}px;
-  line-height: 40px;
+  line-height: ${getFontSize(40)}px;
 `;
 
 export const LargeTitle = styled(SemiBold)`
   font-size: ${getFontSize(24)}px;
-  line-height: 32px;
+  line-height: ${getFontSize(32)}px;
 `;
 
 export const Title = styled(SemiBold)`
   font-size: ${getFontSize(20)}px;
-  line-height: 28px;
+  line-height: ${getFontSize(28)}px;
 `;
 
 export const SemiLabel = styled(SemiBold)`
   font-size: ${getFontSize(17)}px;
-  line-height: 24px;
+  line-height: ${getFontSize(24)}px;
 `;
 
 export const SemiBody = styled(SemiBold)`
   font-size: ${getFontSize(15)}px;
-  line-height: 24px;
+  line-height: ${getFontSize(24)}px;
 `;
