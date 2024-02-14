071cf3ddbeece30e5dc7dda1b77696cef84df034
Doyun Yu <doyun0109@icloud.com>
2024-02-14 19:49:50 +0900

    Feat: useAppTheme themeMode추가

--- src/hooks/useAppTheme.tsx
+++ src/hooks/useAppTheme.tsx
@@ -1,33 +1,36 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { Appearance, useColorScheme } from 'react-native';
-
-type Theme = 'light' | 'dark' | 'system';
+import ThemeMode from '@lib/type/ThemeMode';
 
 const useAppTheme = () => {
-  const setTheme = (theme: Theme) => {
+  const setTheme = (theme: ThemeMode) => {
     theme === 'system'
       ? Appearance.setColorScheme(null)
       : Appearance.setColorScheme(theme);
-    AsyncStorage.setItem('theme', theme).then(() =>
-      console.log('theme set', theme),
-    );
+    setThemeMode(theme as ThemeMode);
+    AsyncStorage.setItem('theme', theme).then(() => {
+      console.log('theme set', theme);
+    });
   };
 
   useEffect(() => {
     AsyncStorage.getItem('theme').then((value) => {
       if (value === 'system' || !value) {
         setTheme('system');
+        setThemeMode('system');
       } else {
-        setTheme(value as Theme);
+        setTheme(value as ThemeMode);
+        setThemeMode(value as ThemeMode);
       }
     });
   }, []);
-
   const theme = useColorScheme();
+  const [themeMode, setThemeMode] = useState<ThemeMode>();
 
   return {
     theme,
+    themeMode,
     setTheme,
   };
 };
