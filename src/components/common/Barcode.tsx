969d0e25645ce38356a597e46257496b599ff7e6
Hyeonmeoung Joo <wugusaud77@icloud.com>
2024-02-17 07:54:09 +0900

    Refactor: binaryToSvg 함수 분리

--- src/components/common/Barcode.tsx
+++ src/components/common/Barcode.tsx
@@ -1,11 +1,10 @@
 import React, { useState } from 'react';
-
 import barcodes from 'jsbarcode/src/barcodes';
-
-import { Surface, Shape } from '@react-native-community/art';
 import { useEffect } from 'react';
 import { View } from 'react-native';
 import { useTheme } from 'styled-components/native';
+import { Path, Svg } from 'react-native-svg';
+import { binaryToSvg } from '@/utils/binaryToSvg';
 
 interface BarcodeProps {
   value: string;
@@ -18,48 +17,23 @@ const Barcode = ({ value, format = 'CODE128' }: BarcodeProps) => {
   const [width, setWidth] = useState<number>();
   useEffect(() => {
     const encoded = new barcodes[format](value, {}).encode();
-    setBars(drawSvgBarCode(encoded));
+    setBars(binaryToSvg(encoded));
     setWidth(encoded.data.length * 2);
-    // console.log(encoded);
+    console.log(bars);
   }, [format, value]);
   return (
     <View
       style={{
         alignItems: 'center',
-        // backgroundColor: 'red',
       }}
     >
-      <Surface height={48} width={width}>
-        <Shape d={bars} fill={colors.gray80} />
-      </Surface>
+      <Svg height={48} width={width}>
+        {bars?.map((bar: string, index: number) => (
+          <Path key={index} d={bar} fill={colors.gray80} />
+        ))}
+      </Svg>
     </View>
   );
-};
-
-const drawSvgBarCode = (encoding: any) => {
-  const rects = [];
-  // binary data of barcode
-  const binary = encoding.data;
-
-  let barWidth = 0;
-  let x = 0;
-  for (let i = 0; i < binary.length; i++) {
-    x = i * 2;
-    if (binary[i] === '1') {
-      barWidth++;
-    } else if (barWidth > 0) {
-      rects.push(
-        `M${x - 2 * barWidth},${0}h${2 * barWidth}v${100}h-${2 * barWidth}z`,
-      );
-      barWidth = 0;
-    }
-  }
-  if (barWidth > 0) {
-    rects.push(
-      `M${x - 2 * barWidth},${0}h${2 * barWidth}v${100}h-${2 * barWidth}z`,
-    );
-  }
-  return rects;
 };
 
 export default Barcode;
