5e1af9030a747c1cf2c22b84a2c03db98dacf0e0
Hyeonmeoung Joo <wugusaud77@icloud.com>
2024-02-21 07:07:04 +0900

    Fix: Removed unused function useEffect

--- src/screens/SettingScreen.tsx
+++ src/screens/SettingScreen.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import LayoutWithHeader from '@components/layout/LayoutWithHeader';
 import { useNavigation } from '@react-navigation/native';
 import { Column, Row } from '@components/atomic';
