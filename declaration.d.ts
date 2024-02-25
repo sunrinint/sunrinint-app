dc52bfc27720b3247e8afeadf3c8eb8b7309f727
Doyun Yu <doyun0109@icloud.com>
2024-02-25 09:11:34 +0900

    Refactor: Env type

--- declaration.d.ts
+++ declaration.d.ts
@@ -7,4 +7,8 @@ declare module '*.svg' {
 
 declare module '@env' {
   export const SERVER_URL: string;
+  export const PUBLIC_API_URL: string;
+  export const GOOGLE_CLIENT_ID: string;
+  export const GOOGLE_IOS_CLIENT_ID: string;
+  export const GOOGLE_CLIENT_SECRET: string;
 }
