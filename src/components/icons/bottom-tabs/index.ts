c718315b66c56ba96df0f1ed01446f11365d63c1
jureuk7 <jureuk7@protonmail.com>
2024-04-30 15:04:37 +0900

    Feat: edit bottom tab bar style

--- src/components/icons/bottom-tabs/index.ts
+++ src/components/icons/bottom-tabs/index.ts
@@ -1,3 +1,4 @@
 export { default as HomeIcon } from './HomeIcon';
 export { default as ClubIcon } from './ClubIcon';
 export { default as SchoolCardIcon } from './SchoolCardIcon';
+export { default as SettingsIcon } from './SettingsIcon';
--- src/components/layout/BottomTabBar.tsx
+++ src/components/layout/BottomTabBar.tsx
@@ -1,108 +1,66 @@
 import React from 'react';
-import styled, { useTheme } from 'styled-components/native';
-import { Animated, Platform, Pressable } from 'react-native';
+import styled from 'styled-components/native';
+import { Platform } from 'react-native';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
-import { Spacer } from '@components/atomic/Spacer';
 import Typography from '@components/typography';
-import { Shadow } from 'react-native-shadow-2';
 
 const BottomTabBar = ({ state, descriptors, navigation }: any) => {
   const { bottom } = useSafeAreaInsets();
-  const { colors } = useTheme();
+  const bottomInset = bottom === 0 ? 15 : bottom;
   return (
-    <Wrapper>
-      <Shadow
-        distance={10}
-        startColor={colors.shadow1}
-        offset={[0, 12]}
-        style={{
-          borderRadius: 60,
-        }}
-      >
-        <Container>
-          {state.routes.map((route: any, index: number) => {
-            const isFocused = state.index === index;
-            const { options } = descriptors[route.key];
-            const label = options.tabBarLabel;
-            const Icon = options.tabBarIcon;
-            const color = isFocused
-              ? options.tabBarActiveTintColor
-              : options.tabBarInactiveTintColor;
-            const onPress = () => {
-              const event = navigation.emit({
-                type: 'tabPress',
-                target: route.key,
-              });
-              if (!isFocused && !event.defaultPrevented) {
-                navigation.navigate(route.name);
-              }
-            };
-            return (
-              <ItemContainer
-                key={index}
-                onPress={onPress}
-                $active={isFocused}
-                style={({ pressed }) => ({
-                  backgroundColor: isFocused
-                    ? colors.gray90
-                    : pressed
-                      ? colors.gray20
-                      : colors.gray10,
-                })}
-              >
-                <Icon color={color} focused={isFocused} />
-                {isFocused && (
-                  <Typography.SemiBody $color={color}>
-                    {label}
-                  </Typography.SemiBody>
-                )}
-              </ItemContainer>
-            );
-          })}
-        </Container>
-      </Shadow>
-      {Platform.OS === 'android' && <Spacer $height={15} />}
-      <Spacer $height={bottom} />
-    </Wrapper>
+    <Container
+      style={{
+        paddingBottom: Platform.OS === 'ios' ? bottomInset : bottomInset,
+      }}
+    >
+      {state.routes.map((route: any, index: number) => {
+        const isFocused = state.index === index;
+        const { options } = descriptors[route.key];
+        const label = options.tabBarLabel;
+        const Icon = options.tabBarIcon;
+        const color = isFocused
+          ? options.tabBarActiveTintColor
+          : options.tabBarInactiveTintColor;
+        const onPress = () => {
+          const event = navigation.emit({
+            type: 'tabPress',
+            target: route.key,
+          });
+          if (!isFocused && !event.defaultPrevented) {
+            navigation.navigate(route.name);
+          }
+        };
+        return (
+          <ItemContainer key={index} onPress={onPress}>
+            <Icon color={color} focused={isFocused} />
+            <Typography.Caption $color={color}>{label}</Typography.Caption>
+          </ItemContainer>
+        );
+      })}
+    </Container>
   );
 };
 
-const Wrapper = styled.View`
-  position: absolute;
-  width: 100%;
-  align-items: center;
-  justify-content: center;
-  padding: 8px 0 0 0;
-  background-color: transparent;
-  bottom: 0;
-`;
-
 const Container = styled.View`
-  width: 232px;
-  height: 64px;
+  width: 100%;
   background-color: ${(props) => props.theme.colors.gray10};
-  border-radius: 60px;
   display: flex;
-  padding: 8px;
-  gap: 6px;
   flex-direction: row;
-  border: 1px solid ${(props) => props.theme.colors.gray30};
+  padding: 8px 12px;
+  justify-content: space-between;
+  align-items: center;
+  align-self: stretch;
+  border-top-width: 1px;
+  border-color: ${(props) => props.theme.colors.gray30};
 `;
 
-const AnimateTab = Animated.createAnimatedComponent(Pressable);
-
-const ItemContainer = styled.Pressable<{ $active: boolean }>`
+const ItemContainer = styled.Pressable`
   display: flex;
-  flex-direction: row;
-  gap: 12px;
-  ${(props) => props.$active && 'width: 108px'};
+  flex-direction: column;
   justify-content: center;
   align-items: center;
-  flex: ${(props) => (props.$active ? 1 : 'none')};
-  padding: 0 12px;
-  background-color: ${(props) =>
-    props.$active ? props.theme.colors.gray90 : 'transparent'};
-  border-radius: 50px;
+  gap: 4px;
+  flex: 1 0 0;
 `;
 
 export default BottomTabBar;
