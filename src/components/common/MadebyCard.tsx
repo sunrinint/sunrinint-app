a66c7cb199acdebfcf8f04e0f6b50f96d6f703a3
jureuk7 <jureuk7@protonmail.com>
2024-05-20 11:20:24 +0900

    Refactor: madeby screen

--- src/components/common/MadebyCard.tsx
+++ src/components/common/MadebyCard.tsx
@@ -1,52 +1,29 @@
 import styled, { useTheme } from 'styled-components/native';
-import { Row } from '@components/atomic';
-import { View } from 'react-native';
+import { Column } from '@components/atomic';
+import { Linking } from 'react-native';
 import Typography from '@components/typography';
-import WebsiteIcon from '@assets/icons/website.svg';
-import InstagramIcon from '@assets/icons/instagram.svg';
-import FacebookIcon from '@assets/icons/facebook.svg';
 import React from 'react';
-import { Body } from '@components/typography/Regular';
 
-interface MadebyProps{
-  logo: string;
+interface MadebyProps {
+  logo: { uri: string };
   name: string;
   role: string;
-  website: string;
-  facebook: string;
-  instagram: string;
+  link: string;
 }
 
-const MadebyCard = ({logo, name, role, website, facebook, instagram}: MadebyProps) => {
+const MadebyCard = ({ logo, name, role, link }: MadebyProps) => {
   const { colors } = useTheme();
   return (
-    <ClubCardLayout>
-      <ClubCardTop>
-        <Row $fill $justifyContent={'space-between'} $alignItems={'center'}>
-          <Row $gap={8} $alignItems={'center'}>
-            <Logo source={{ uri: logo }} />
-            <View>
-              <Typography.Label $color={colors.gray80}>
-                {name}
-              </Typography.Label>
-              <Typography.Body $color={colors.gray80}>
-                {role}
-              </Typography.Body>
-            </View>
-          </Row>
-        </Row>
-      </ClubCardTop>
-      <ButtonGroup>
-        <RoundedButton disabled={!website}>
-          <WebsiteIcon fill={website ? colors.gray80 : colors.gray40} />
-        </RoundedButton>
-        <RoundedButton disabled={!facebook}>
-          <FacebookIcon fill={facebook ? colors.gray80 : colors.gray40} />
-        </RoundedButton>
-        <RoundedButton disabled={!instagram}>
-          <InstagramIcon fill={instagram ? colors.gray80 : colors.gray40} />
-        </RoundedButton>
-      </ButtonGroup>
+    <ClubCardLayout
+      onPress={() => {
+        Linking.openURL(link);
+      }}
+    >
+      <Logo source={logo || { uri: '' }} />
+      <Column $gap={2}>
+        <Typography.Label $color={colors.gray80}>{name}</Typography.Label>
+        <Typography.Body $color={colors.gray60}>{role}</Typography.Body>
+      </Column>
     </ClubCardLayout>
   );
 };
@@ -56,48 +33,14 @@ export default MadebyCard;
 const Logo = styled.Image`
   width: 48px;
   height: 48px;
-  //border-radius: 8px;
+  border-radius: 24px;
 `;
 
-const ButtonGroup = styled.View`
+const ClubCardLayout = styled.TouchableOpacity`
   display: flex;
   flex-direction: row;
-  gap: 8px;
   padding: 12px;
-`;
-
-const RoundedButton = styled.TouchableOpacity`
-  flex: 1;
-  height: 44px;
-  border-radius: 96px;
-  background-color: ${(props) => props.theme.colors.gray30};
-  justify-content: center;
   align-items: center;
-`;
-
-const ClubCardLayout = styled.View`
-  padding: 8px;
-  background-color: ${(props) => props.theme.colors.gray10};
-  border-radius: 8px;
-  border: 1px solid ${(props) => props.theme.colors.gray30};
+  gap: 12px;
   align-self: stretch;
 `;
-
-const ClubCardTop = styled.View`
-  padding: 12px;
-  gap: 16px;
-  display: flex;
-`;
-
-
-
-
-const MadebyCardLayout = styled.View`
-  padding: 8px;
-  background-color: ${(props) => props.theme.colors.gray10};
-  border-radius: 8px;
-  border: 1px solid ${(props) => props.theme.colors.gray30};
-  align-self: stretch;
-`;
-
-
--- src/screens/MadebyScreen.tsx
+++ src/screens/MadebyScreen.tsx
@@ -1,36 +1,198 @@
-import React, { Suspense } from 'react';
+import React from 'react';
 import LayoutWithHeader from '@/components/layout/LayoutWithHeader';
 import styled, { useTheme } from 'styled-components/native';
 import MadebyCard from '@components/common/MadebyCard';
 import Typography from '@components/typography';
 import { ScrollView } from 'react-native';
-import { Spacer } from '@components/atomic';
 
+const developers = [
+  {
+    name: '김성빈',
+    role: '1기 PO, Backend Developer',
+    link: 'https://github.com/plebea',
+    logo: require('@assets/images/ksb.png'),
+  },
+  {
+    name: '주현명',
+    role: '1기 PM, Frontend Developer',
+    link: 'https://github.com/jureuk7',
+    logo: require('@assets/images/jureuk.png'),
+  },
+  {
+    name: '유도윤',
+    role: 'Frontend Developer',
+    link: 'https://github.com/doyun0109',
+    logo: require('@assets/images/doyun.png'),
+  },
+  {
+    name: '이정우',
+    role: 'Frontend Developer',
+    link: 'https://github.com/paul3083',
+    logo: require('@assets/images/jwoo.png'),
+  },
+  {
+    name: '홍예훈',
+    role: 'Frontend Developer',
+    link: 'https://github.com/hoonih',
+    logo: require('@assets/images/yehoon.png'),
+  },
+  {
+    name: '박금혁',
+    role: 'Backend Developer',
+    link: 'https://github.com/rmagur1203',
+    logo: require('@assets/images/gold.png'),
+  },
+];
+
+const Designers = [
+  // 백시현 박시원 조현우 이주영 표한빈 김대현
+  {
+    name: '백시현',
+    role: 'UI/UX Designer',
+    link: 'https://behance.net/sihyunlights',
+    logo: require('@assets/images/sihyunlights.png'),
+  },
+  {
+    name: '박시원',
+    role: 'UI/UX Designer',
+    link: 'https://behance.net/whoisapple',
+    logo: require('@assets/images/siwon.png'),
+  },
+  {
+    name: '조현우',
+    role: 'UI/UX Designer',
+    link: 'https://behance.net/hyunwoocho',
+    logo: require('@assets/images/hyunwoocho.png'),
+  },
+  {
+    name: '이주영',
+    role: 'UI/UX Designer',
+    link: 'https://behance.net/juyounglee',
+    logo: require('@assets/images/jooyong.png'),
+  },
+  {
+    name: '표한빈',
+    role: 'UI/UX Designer',
+    link: 'https://behance.net/hanbin81j30d32',
+    logo: require('@assets/images/sharpbin.png'),
+  },
+  {
+    name: '김대현',
+    role: 'UI/UX Designer',
+    link: 'https://behance.net/daehyunkim',
+    logo: require('@assets/images/dhyun.png'),
+  },
+];
+
+const Directors = [
+  //오유성 남영재
+  {
+    name: '오유성',
+    role: 'Director',
+    link: 'https://github.com/oyshallo562',
+    logo: require('@assets/images/oyshallo.png'),
+  },
+  {
+    name: '남영재',
+    role: 'Director',
+    link: 'https://github.com/ye0ngjae',
+    logo: require('@assets/images/yj.png'),
+  },
+];
+
+const SpecialThanks = [
+  // 조성주 권지원
+  {
+    name: '조성주',
+    role: '급식 API 제공',
+    link: 'https://github.com/iamfiro',
+    logo: require('@assets/images/firo.png'),
+  },
+  {
+    name: '권지원',
+    role: '급식 API 제공',
+    link: 'https://github.com/jwkwon0817',
+    logo: require('@assets/images/jiwon.png'),
+  },
+];
 
 const MadebyScreen = () => {
   const { colors } = useTheme();
   return (
     <LayoutWithHeader title="만든 사람들" showBack>
       <Container>
-        <ScrollView showsVerticalScrollIndicator={false}>
-          <TitleCardLayout>
-            <Typography.Label $color={colors.gray80}>SunrinINT 개발진</Typography.Label>
-            <Typography.Body $color={colors.gray60}>SunrinINT를 만든 사람들입니다.</Typography.Body>
-          </TitleCardLayout>
-          <Spacer $height={12} />
-          <MadebyCard logo={'@/assets/icons/logo.svg'} name={'김성빈'} role={'PO, Backend Developer'} website={'test'} facebook={'test'} instagram={'test'}/>
-          <Spacer $height={12} />
-          <MadebyCard logo={'@/assets/icons/logo.svg'} name={'김성빈'} role={'PO, Backend Developer'} website={'test'} facebook={'test'} instagram={'test'}/>
-          <Spacer $height={12} />
-          <MadebyCard logo={'@/assets/icons/logo.svg'} name={'김성빈'} role={'PO, Backend Developer'} website={'test'} facebook={'test'} instagram={'test'}/>
-          <Spacer $height={12} />
-          <MadebyCard logo={'@/assets/icons/logo.svg'} name={'김성빈'} role={'PO, Backend Developer'} website={'test'} facebook={'test'} instagram={'test'}/>
-          <Spacer $height={12} />
-          <MadebyCard logo={'@/assets/icons/logo.svg'} name={'김성빈'} role={'PO, Backend Developer'} website={'test'} facebook={'test'} instagram={'test'}/>
-          <Spacer $height={12} />
-          <MadebyCard logo={'@/assets/icons/logo.svg'} name={'김성빈'} role={'PO, Backend Developer'} website={'test'} facebook={'test'} instagram={'test'}/>
-          <Spacer $height={12} />
-          <MadebyCard logo={'@/assets/icons/logo.svg'} name={'김성빈'} role={'PO, Backend Developer'} website={'test'} facebook={'test'} instagram={'test'}/>
+        <ScrollView
+          contentContainerStyle={{
+            gap: 12,
+            paddingBottom: 50,
+            marginTop: 12,
+          }}
+          showsVerticalScrollIndicator={false}
+        >
+          <Card>
+            <CardTop>
+              <Typography.SemiLabel $color={colors.gray80}>
+                개발자
+              </Typography.SemiLabel>
+            </CardTop>
+            {developers.map((developer, index) => (
+              <MadebyCard
+                key={index}
+                logo={developer.logo}
+                name={developer.name}
+                role={developer.role}
+                link={developer.link}
+              />
+            ))}
+          </Card>
+          <Card>
+            <CardTop>
+              <Typography.SemiLabel $color={colors.gray80}>
+                디자이너
+              </Typography.SemiLabel>
+            </CardTop>
+            {Designers.map((developer, index) => (
+              <MadebyCard
+                key={index}
+                logo={developer.logo}
+                name={developer.name}
+                role={developer.role}
+                link={developer.link}
+              />
+            ))}
+          </Card>
+          <Card>
+            <CardTop>
+              <Typography.SemiLabel $color={colors.gray80}>
+                기획자
+              </Typography.SemiLabel>
+            </CardTop>
+            {Directors.map((developer, index) => (
+              <MadebyCard
+                key={index}
+                logo={developer.logo}
+                name={developer.name}
+                role={developer.role}
+                link={developer.link}
+              />
+            ))}
+          </Card>
+          <Card>
+            <CardTop>
+              <Typography.SemiLabel $color={colors.gray80}>
+                Special
+              </Typography.SemiLabel>
+            </CardTop>
+            {SpecialThanks.map((developer, index) => (
+              <MadebyCard
+                key={index}
+                logo={developer.logo}
+                name={developer.name}
+                role={developer.role}
+                link={developer.link}
+              />
+            ))}
+          </Card>
         </ScrollView>
       </Container>
     </LayoutWithHeader>
@@ -39,17 +201,27 @@ const MadebyScreen = () => {
 
 export default MadebyScreen;
 
-const TitleCardLayout = styled.View`
-  padding: 20px;
+const CardTop = styled.View`
+  display: flex;
+  padding: 12px;
+  gap: 12px;
+  align-self: stretch;
+`;
+
+const Card = styled.View`
   background-color: ${(props) => props.theme.colors.gray10};
-  border-radius: 8px;
-    gap: 4px;
   border: 1px solid ${(props) => props.theme.colors.gray30};
+  display: flex;
+  padding: 8px;
+  border-radius: 8px;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
   align-self: stretch;
 `;
 
 const Container = styled.View`
   flex: 1;
-  padding: 12px 12px 0 12px;
+  padding: 0 12px 0 12px;
   gap: 12px;
 `;
