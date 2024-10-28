bec491b23c76e7c80c9325a21ae9671a0ace9e46
hoonih <hoonih1004@gmail.com>
2024-10-28 23:36:25 +0900

    fix: first screen login function error

--- src/lib/api/auth.ts
+++ src/lib/api/auth.ts
@@ -5,8 +5,15 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 export const login = async () => {
   await GoogleSignin.hasPlayServices();
   const { idToken } = await GoogleSignin.signIn();
-  const res = await defaultClient.post('auth/app/google', {
-    idToken: idToken,
-  });
-  return AsyncStorage.setItem('refresh', res.data.refreshToken);
+  try {
+    console.log("Sending ID Token to server:", idToken);
+    const res = await defaultClient.post('auth/app/google', {
+      idToken: idToken,
+    });
+    console.log("Server response:", res.data);  // This will log the server response data if successful.
+    return AsyncStorage.setItem('refresh', res.data.refreshToken);
+  } catch (error) {
+    console.error("Server error:", error.response ? error.response.data : error.message); // Log only the server error
+    throw error;
+  }
 };
